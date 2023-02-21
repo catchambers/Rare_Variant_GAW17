f0c831e85d3155a83ab37d8a7cfb8675b3d2a509
Caitlyn Chitwood <caitlynlchambers@gmail.com>
2023-02-21 10:22:09 -0600

    Initial commit

---
+++ read_in.R
@@ -0,0 +1,7 @@
+#_______________________________________________________________________________
+#                             READ IN DATA FILES
+# PURPOSE: READING IN SAS FILES AND CREATING RMD FOR FURTHER PROCESSING
+# AUTHOR: CAITLYN CHITWOOD
+#_______________________________________________________________________________
+
+
